105f7cc48a4ac294a42a7c3a60b7d68755570347
Benaka prasad M <155962113+Benakaprasad@users.noreply.github.com>
2024-10-05 13:40:41 +0530

    Update hotelbill.java

--- hotelbill.java
+++ hotelbill.java
@@ -1,1 +1,83 @@
-hello bro long time no see
+import java.util.*;
+import java.io.*;
+class hotel
+{
+    public static void main(string args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int ch;
+       do
+      {
+        system.out.println("\t\t\t\t\t\t\tWELCOME TO SAGAR RESTAURANT\t\t\t\t\t");
+        system.out.println("**************************************************");
+        system.out.println("enter 1 for menu");
+        system.out.println("enter 2 for placing the order");
+        system.out.println("enter 3 for service time");
+        system.out.println("enter 4 billing");
+        system.out.println("enter 5 for feedback");
+        system.out.println("enter 6 for exit");
+        system.out.println("enter your choice\n\n");
+        ch=in.nextline();
+        switch(ch)
+        {
+          case 1:
+          {
+          system.out.println("MENU");
+          system.out.println("STARTERS");
+          system.out.println("***************************************");
+          system.out.println("CHICKEN MANCHOW SOUP-----------------\t\t100\-");
+          system.out.println("SWEET CORN CHICKEN SOUP-----------------\t\t110\-");
+          system.out.println("HOT AND SOUR CHICKEN SOUP-----------------\t\t110\-");
+          system.out.println("MAIN FOOD");
+          system.out.println("***************************************");
+          system.out.println("BUTTER ROTI-----------------\t\t30\-");
+          system.out.println("BUTTER KHULCHA-----------------\t\t35\-");
+          system.out.println("BUTTER CHICKEN-----------------\t\t165\-");
+          system.out.println("PALAK PANEER-----------------\t\t$120\-");
+          system.out.println("CHICKEN BIRIYANI-----------------\t\t145\-");
+          system.out.println("MUTTON BIRIYANI-----------------\t\t130\-");
+          system.out.println("CHICKEN CHOPS-----------------\t\t165\-");
+          system.out.println("CHCIKEN PEPPER FRY-----------------\t\t$120\-");
+          system.out.println("CHICKEN ROAST-----------------\t\t145\-");
+          system.out.println("CHICKEN TANDOORI-----------------\t\t130\-");
+          system.out.println("CURRIES");
+          system.out.println("***************************************");
+          system.out.println("PARADISE SPECIAL CHICKEN-----------------\t\t130\-");
+          system.out.println("CHICKEN TIKKA MASALA-----------------\t\t135\-");
+          system.out.println("KADAI CHICKEN-----------------\t\t165\-");
+          system.out.println("MURGH MUSALLAM-----------------\t\t$120\-");
+          system.out.println("CHICKEN NAWABI-----------------\t\t145\-");
+          system.out.println("CHICKEN KANDHARI-----------------\t\t130\-");
+          system.out.println("MUTTON ROGAN JOSH-----------------\t\t165\-");
+          system.out.println("KADAI GOSHT-----------------\t\t$120\-");
+          system.out.println("DESSERTS");
+          system.out.println("***************************************");
+          system.out.println("CHOCOLATE-----------------\t\t70\-");
+          system.out.println("BUTTER SCOTCH-----------------\t\t$95\-");
+          system.out.println("VANILLA WITH HOT CHOCOLATE SAUCE-----------------\t\t75\-");
+          system.out.println("TRIPLE SUNDAE-----------------\t\t80\-");
+          system.out.println("BROWNEY WITH ICE CREAM/CHOCOLATE-----------------\t\t60\-");
+          system.out.println("TOOTI FRUTTY-----------------\t\t55\-");
+          system.out.println("SALADS");
+          system.out.println("***************************************");
+          system.out.println("FRESH GARDEN SALAD-----------------\t\t30");
+          system.out.println("GREEK SALAD-----------------\t\t35");
+          system.out.println("RUSSIAN SALAD-----------------\t\t40");
+          system.out.println("TOSSED GREEN SALAD-----------------\t\t20");
+          break;
+         }
+         case 2:
+         {
+            
+
+         }
+
+        
+
+                                
+
+                                            
+
+        
+    }
+}
