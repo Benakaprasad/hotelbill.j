9864ef9f10be66c9b011acf7b21ed56d27aea66a
Benaka prasad M <155962113+Benakaprasad@users.noreply.github.com>
2024-10-07 23:29:58 +0530

    Update hotelbill.java

--- hotelbill.java
+++ hotelbill.java
@@ -9,16 +9,16 @@ class hotel
       
       do
       {
-        System.out.println("\t\t\t\t\t\t\tWELCOME TO SAGAR RESTAURANT\t\t\t\t\t");
-        System.out.println("******************");
-        System.out.println("enter 1 for menu");
-        System.out.println("enter 2 for placing the order");
-        System.out.println("enter 3 for service time");
-        System.out.println("enter 4 billing");
-        System.out.println("enter 5 for feedback");
-        System.out.println("enter 6 for exit");
-        System.out.println("enter your choice\n\n");
-        ch=in.nextInt();
+        system.out.println("\t\t\t\t\t\t\tWELCOME TO SAGAR RESTAURANT\t\t\t\t\t");
+        system.out.println("******************");
+        system.out.println("enter 1 for menu");
+        system.out.println("enter 2 for placing the order");
+        system.out.println("enter 3 for service time");
+        system.out.println("enter 4 billing");
+        system.out.println("enter 5 for feedback");
+        system.out.println("enter 6 for exit");
+        system.out.println("enter your choice\n\n");
+        ch=in.nextline();
         switch(ch)
         {
           case 1:
@@ -116,7 +116,7 @@ class hotel
             int item9=145;
             int item10=130;
             System.out.println("enter your number of choice from main food:");
-            System.out.println("enter 11 total bill for starters");
+            System.out.println("enter 11 total bill for main food");
             int size1=in.nextInt();
              switch(size1)
             {
@@ -188,10 +188,177 @@ class hotel
                 System.out.println("invalid choice");
                 break;
           }
-            
+           double total14,total15,total16,total17,total18,total19,total20,total21;
+            int item11=130;
+            int item12=135;
+            int item13=165;
+            int item14=120;
+            int item15=145;
+            int item16=130;
+            int item17=165;
+            int item18=120;            
+            System.out.println("enter your number of choice from curries:");
+            System.out.println("enter 9 total bill for curries");
+            int size2=in.nextInt();
+             switch(size2)
+            {
+                case 1:
+                System.out.println("chicken manchow soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total14=item11*quantity;
+                break;
+                case 2:
+                System.out.println("sweet corn chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total15=item12*quantity;
+                break;
+                case 3:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total16=item13*quantity;
+                break;
+                case 4:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total17=item14*quantity;
+                break;
+                case 5:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total18=item15*quantity;
+                break;
+                case 6:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total19=item16*quantity;
+                break;
+                case 7:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total20=item17*quantity;
+                break;
+                case 8:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total21=item18*quantity;
+                break;
+                case 9:
+                double sum1=total14+total15+total16+total17+total18+total19+total20+total21;
+                System.out.println("total bill for main food"+sum1);
+                break;
+                default:                
+                System.out.println("invalid choice");
+                break;
+          } 
+            double total22,total23,total24,total24,total26,total27;
+            int item19=130;
+            int item20=135;
+            int item21=165;
+            int item22=120;
+            int item23=145;
+            int item24=130;
+            System.out.println("enter your number of choice from desserts:");
+            System.out.println("enter 7 total bill for desserts");
+            int size3=in.nextInt();
+             switch(size3)
+            {
+                case 1:
+                System.out.println("chicken manchow soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total22=item19*quantity;
+                break;
+                case 2:
+                System.out.println("sweet corn chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total23=item20*quantity;
+                break;
+                case 3:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total24=item21*quantity;
+                break;
+                case 4:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total25=item22*quantity;
+                break;
+                case 5:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total26=item23*quantity;
+                break;
+                case 6:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total27=item24*quantity;
+                break;
+                case 7:
+                double sum2=total22+total23+total24+total25+total26+total27;
+                System.out.println("total bill for main food"+sum2);
+                break;
+                default:                
+                System.out.println("invalid choice");
+                break;
+          }      
+             double total28,total29,total30,total31;
+            int item25=30;
+            int item26=35;
+            int item27=40;
+            int item28=20;
+            System.out.println("enter your number of choice from salads:");
+            System.out.println("enter 5 total bill for salads");
+            int size4=in.nextInt();
+             switch(size4)
+            {
+                case 1:
+                System.out.println("chicken manchow soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total28=item25*quantity;
+                break;
+                case 2:
+                System.out.println("sweet corn chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total29=item26*quantity;
+                break;
+                case 3:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total30=item27*quantity;
+                break;
+                case 4:
+                System.out.println("hot and sour chicken soup:");
+                System.out.println("enter the quantity:");
+                int quantity=in.nextInt();
+                total31=item28*quantity;
+                break;
+                case 5:
+                double sum3=total22+total23+total24+total25;
+                System.out.println("total bill for salads"+sum3);
+                break;
+                break;
                 
-        
-
+                default:                
+                System.out.println("invalid choice");
+                break;
+          }   
+              
                  
                 
         
